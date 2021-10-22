a1461e1629ec54ee13626a532cc98080601b852e
prkm93 <aradshub93@gmail.com>
2021-10-22 09:32:08 +0530

    Added navbar app

--- 4-express-tutorials/app.js
+++ 4-express-tutorials/app.js
@@ -2,7 +2,10 @@ const http = require('http');
 const {readFileSync} = require('fs');
 
 // get all files 
-const homePage = readFileSync('./index.html');
+const homePage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeLogo = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req, res) => {
     console.log('user hit the server');
@@ -21,6 +24,22 @@ const server = http.createServer((req, res) => {
         res.write('<h1>About page</h1>');
         res.end();
     }
+    // styles file
+    else if (url === "/styles.css") {
+        res.writeHead(200, {'content-type':'text/css'});
+        res.write(homeStyles);
+        res.end();
+    } 
+    else if (url === "/logo.svg") {
+        res.writeHead(200, {'content-type':'image/svg+xml'});
+        res.write(homeLogo);
+        res.end();
+    }
+    else if (url === "/browser-app.js") {
+        res.writeHead(200, {'content-type':'text/javascript'});
+        res.write(homeLogic);
+        res.end();
+    } 
     // 404  
     else {
         res.writeHead(404, {'content-type': 'text/html'});
