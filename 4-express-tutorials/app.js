985d21017035bdb86021ac3400fcb6228793788d
prkm93 <aradshub93@gmail.com>
2021-10-22 09:04:05 +0530

    Added navbar-app

--- 4-express-tutorials/app.js
+++ 4-express-tutorials/app.js
@@ -1,10 +1,32 @@
 const http = require('http');
+const {readFileSync} = require('fs');
+
+// get all files 
+const homePage = readFileSync('./index.html');
 
 const server = http.createServer((req, res) => {
     console.log('user hit the server');
-    res.writeHead(200, {'content-type': 'text/html'}); // passing text/plain renders as plain text
-    res.write('<h1>Home Page</h1>');
-    res.end();
+    console.log(req.method);
+    console.log(req.url);
+    const url = req.url;
+    //home page
+    if (url === "/") {
+        res.writeHead(200, {'content-type': 'text/html'}); // passing text/plain renders as plain text
+        res.write(homePage);
+        res.end();
+    } 
+    // about page
+    else if (url === "/about") {
+        res.writeHead(200, {'content-type':'text/html'});
+        res.write('<h1>About page</h1>');
+        res.end();
+    }
+    // 404  
+    else {
+        res.writeHead(404, {'content-type': 'text/html'});
+        res.write('<h1>404 page not found</h1>');
+        res.end();
+    }
 });
 
 server.listen(3000);
