375fc7e668a9e5c41fb5715ea45f574857eb68f0
prkm93 <aradshub93@gmail.com>
2021-10-25 09:49:25 +0530

    minor refactor

--- 4-express-tutorials/controllers/people.js
+++ 4-express-tutorials/controllers/people.js
@@ -1,4 +1,4 @@
-const { person } = require('../data');
+const { people } = require('../data');
 
 const getPeople = (req, res) => {
     res.status(200).json({success: true, data: people});
--- 4-express-tutorials/routes/people.js
+++ 4-express-tutorials/routes/people.js
@@ -9,14 +9,14 @@ const {
     deletePerson
 } = require('../controllers/people');
 
-router.get('/', getPeople);
+// router.get('/', getPeople);
+// router.post('/', createPerson);
+// router.post('/postman', createPersonPostman);
+// router.put('/:id', updatePerson); 
+// router.delete('/:id', deletePerson);
 
-router.post('/', createPerson);
-
-router.post('/postman', createPersonPostman);
-
-router.put('/:id', updatePerson); 
-
-router.delete('/:id', deletePerson);
+router.route('/').get(getPeople).post(createPerson);
+router.route('/postman').post(createPersonPostman);
+router.route('/:id').put(updatePerson).delete(deletePerson);
 
 module.exports = router;
