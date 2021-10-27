4988a9d802e7cdfeb75734809d78e2ae882e248a
prkm93 <aradshub93@gmail.com>
2021-10-27 09:21:47 +0530

    Added get, post, put and delete functions

--- 5-task-manager/controllers/tasks.js
+++ 5-task-manager/controllers/tasks.js
@@ -1,24 +1,76 @@
+const Task = require('../models/Task');
+
 const { reset } = require("nodemon");
 
-const getAllTasks = (req, res) => {
-    res.send('get all tasks');
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({tasks});
+    } catch (err) {
+        res.status(500).json({err: msg});
+    }
 }
 
-const createTask = (req, res) => {
-    res.json(req.body);
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({ task });
+    } catch (error) {
+        res.status(500).json({msg: error});
+    }
 }
 
-const getTask = (req, res) => {
-    const {id} = req.params;
-    res.json({id});
+const getTask = async (req, res) => {
+
+    try {
+
+        const { id: taskId } = req.params; //aliasing id to taskId
+        const task = await Task.findOne({ _id : taskId });
+
+        // if task not found
+        if (!task) {
+            return res.status(404).json({msg: `No task with id :${taskId} `})
+        }
+
+        res.status(200).json({task});
+    } catch (err) {
+        res.status(500).json({msg:err});
+    }
 }
 
-const updateTask = (req, res) => {
-    res.send('update task');
+const updateTask = async (req, res) => {
+    try {
+        const {id: taskId} = req.params;
+        const updatedTask = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+            new: true,
+            runValidators: true
+        }); // the options (3rd argument) is used to display the updated result in response body, though the field  is updated but it doesn't display in response.
+        
+        if (!updatedTask) {
+            return res.send(404).json({msg: `No task with id: ${taskId}`});
+        }
+
+        res.status(200).json({updatedTask});
+
+    } catch (err) {
+        res.status(500).json({msg: err});
+    }
 }
 
-const deleteTask = (req, res) => {
-    res.send('delete task');
+const deleteTask = async (req, res) => {
+    try {
+        const {id: taskId} = req.params;
+        const task = await Task.findOneAndDelete({_id: taskId});
+        
+        if (!task) {
+            return res.send(200).json({msg: `no task with id : ${taskId}` });
+        }
+
+        res.status(200).json({task, msg: `task deleted successfully with id :${taskId}`});
+        //  res.status(200).send({task: null, status: 'success'}) // since deleting only deletes item and as per UX, only delete message should be displayed.
+    } catch (err) { 
+        res.send(500).json({msg: err});
+    }
 }
 
 module.exports = {
--- 5-task-manager/routes/tasks.js
+++ 5-task-manager/routes/tasks.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {getAllTasks, getTask, updateTask, createTask, deleteTask} = require('../controllers/tasks');
 
-router.route('/').get(getAllTasks).post(createTask);
+router.route('/').get(getAllTasks).post(createTask); // for same routes, we can chain the methods
 
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
