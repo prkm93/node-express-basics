1d4a72c71d9a155675a7df1f4e8671160f3097d0
prkm93 <aradshub93@gmail.com>
2021-11-10 21:53:35 +0530

    added basic files

--- 6-store-api/app.js
+++ 6-store-api/app.js
@@ -1,1 +1,43 @@
 console.log('04 Store API')
+
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// middlware
+app.use(express.json());
+
+//routes
+app.get('/', (req, res) => {
+    res.send(
+        ('<h1>Store API</h1><a href="/api/v1/products">Products route</a>')
+    )
+})
+
+
+app.use('/api/v1/products');
+
+
+//product route
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async() => {
+    try {
+        // connect DB
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`server is listening on ${port}...`));
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+start();
