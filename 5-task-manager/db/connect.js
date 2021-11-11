c4cc41eb000faf0cf20240979fc162dfb607883b
prkm93 <aradshub93@gmail.com>
2021-11-11 09:20:17 +0530

    update

--- 5-task-manager/db/connect.js
+++ 5-task-manager/db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-// const connectionString = 'mongodb+srv://pradeep:Test123@nodeexpressprojects.hl3pl.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority';  
-
 const connectDB = (url) => {
     return mongoose.connect(url, { 
         useNewUrlParser: true,  //This is for resolving deprecation errors
