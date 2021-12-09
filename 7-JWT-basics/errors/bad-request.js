1718c68864ec6756a789c9a637a12465c3f05f0a
Pradeep <aradshub93@gmail.com>
2021-12-09 09:28:06 +0530

    separating error messages with different class

---
+++ 7-JWT-basics/errors/bad-request.js
@@ -0,0 +1,11 @@
+const CustomAPIError = require('./custom-error');
+const { StatusCodes } = require('http-status-codes');
+
+class BadRequestError extends CustomAPIError {
+    constructor(message) {
+      super(message)
+      this.statusCode = StatusCodes.BAD_REQUEST; // 400
+    }
+  }
+  
+module.exports = BadRequestError;  
