ca95c4e95dbbc47eda2032dbd7f89680b493da73
Pradeep <aradshub93@gmail.com>
2021-12-09 09:15:17 +0530

    Refactored error class

--- 7-JWT-basics/controllers/main.js
+++ 7-JWT-basics/controllers/main.js
@@ -28,23 +28,13 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
-    const authHeader = req.headers.authorization;
     
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new CustomAPIError('No token provided', 401);
-    }
-
-    const token = authHeader.split(' ')[1];
+    const luckyNumber = Math.floor(Math.random()*100);
     
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);     
-
-        const luckyNumber = Math.floor(Math.random()*100);
-        res.status(200).json({msg: `Hello, ${decoded.username}`, secret: `here is authorised data, lucky number ${luckyNumber}`});
-    
-    } catch (err) {
-        throw new CustomAPIError('Not authorized to access this route', 401);
-    }
+    res.status(200).json({
+        msg: `Hello, ${req.user.username}`, 
+        secret: `here is authorised data, lucky number ${luckyNumber}`
+    });
 }
 
 module.exports = {
