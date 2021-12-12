900d8e6d368c5b2e3ad617dd6bbecd1112b64047
Pradeep Maurya <aradshub93@gmail.com>
2021-12-12 09:49:41 +0530

    addew jwt and refactoring of auth.js

--- 8-Jobs-API/controllers/auth.js
+++ 8-Jobs-API/controllers/auth.js
@@ -1,5 +1,25 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+// const { BadRequestError } = require('../errors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const register = async (req, res) => {
-    res.send('register user');
+    
+    // Below functionality is addded in User model as pre Schema
+    /*
+        const {name, email, password} = req.body;
+    
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const tempUser = {name, email, password: hashedPassword};
+    */ 
+
+    const user = await User.create({...req.body});
+    const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', { expiresIn: '30d' });
+
+    res.status(StatusCodes.CREATED).json({ user:{ name: user.getName() }, token});
 }
 
 const login = async (req, res) => {
--- 8-Jobs-API/models/User.js
+++ 8-Jobs-API/models/User.js
@@ -1,9 +1,7 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-
-
- const mongoose = require('mongoose');
-
- const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
      name:{
          type: String,
          required: [true, 'Please provide a name'],
@@ -22,9 +20,18 @@
      password: {
          type: String,
          required: [true, 'Please provide password'],
-         minLength: 6,
-         maxLength: 12
+         minLength: 6
      }
  })
 
+ UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+ })
+
+ UserSchema.methods.getName = function() {
+     return this.name;
+ }
+ 
  module.exports = mongoose.model('User', UserSchema);
